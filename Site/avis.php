c9cc7ae424021e800a7c8b295ea81179a6db4b06
CAUQUIL Tanguy <Tanguy.Cauquil@bordeaux-inp.fr>
2022-12-02 14:17:08 +0100

    opti header

--- Site/avis.php
+++ Site/avis.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        
-		<title>Saxo Gang Bande Dessinée</title>
-		<meta charset="utf-8" />
-		<meta name="description" content="Base de données de 50 jeux de société, avec plus de 200 avis !" />
-		<meta name="author" content="Luca PEREIRA" />
-	</head>
 
     <header>
         <?php include('header.php'); ?> 
